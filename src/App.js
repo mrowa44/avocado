7fad7df50cbb6b7a597e137238ba5c83f45c27e4
Justyna Rachowicz <mrowa44@gmail.com>
2017-06-05 18:03:51 +0200

    Add photon

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className="app window">
+        <div className="app-header">
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p className="app-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
