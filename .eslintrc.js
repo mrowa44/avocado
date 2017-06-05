025165e8081231d64724d4507d2bacd1bbe4f66c
Justyna Rachowicz <mrowa44@gmail.com>
2017-06-05 17:45:53 +0200

    Fix remaining lint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,17 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
+  extends: 'airbnb',
+  env: {
+    browser: true,
+    mocha: true,
+    es6: true,
+  },
+  plugins: [
+    'react',
+    'jsx-a11y',
+    'import'
+  ],
+  rules: {
+    'react/prefer-stateless-function': 0,
+    'react/jsx-filename-extension': 0,
+  },
 };
