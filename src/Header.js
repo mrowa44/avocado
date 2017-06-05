288a90454dec59d5779b064a0b3a0b51786950e1
Justyna Rachowicz <mrowa44@gmail.com>
2017-06-06 00:42:11 +0200

    Focus input on cmd+l

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import Mousetrap from 'mousetrap';
+
 import './Header.css';
 
 class Header extends Component {
+  componentDidMount() {
+    Mousetrap.bind('command+l', () => {
+      this.input.focus();
+    });
+  }
+
   render() {
     return (
       <header className="header toolbar toolbar-header">
         <div className="header-drag" />
 
         <div className="toolbar-actions">
-          <input className="form-control" type="text" placeholder="+ Add a task" />
+          <input
+            className="form-control"
+            type="text"
+            placeholder="+ Add a task"
+            ref={(node) => { this.input = node; }}
+          />
         </div>
       </header>
     );
