6743a924d101e6146b0b58b9dc46ea840c4ee09c
Justyna Rachowicz <mrowa44@gmail.com>
2017-07-10 18:29:07 +0200

    Add cmd+j, cmd+k handling to switch between tasks

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Mousetrap from 'mousetrap';
 
 import {
   FETCHED_TASKS,
@@ -13,6 +14,8 @@ class TaskList extends Component {
   constructor() {
     super();
     this.setActive = this.setActive.bind(this);
+    this.setActiveDown = this.setActiveDown.bind(this);
+    this.setActiveUp = this.setActiveUp.bind(this);
     this.state = {
       tasks: [],
       activeId: null,
@@ -24,10 +27,41 @@ class TaskList extends Component {
     ipc.on(FETCHED_TASKS, (event, tasks) => {
       this.setState({ tasks });
     });
+
+    Mousetrap.bind('command+j', this.setActiveDown);
+    Mousetrap.bind('command+k', this.setActiveUp);
   }
 
   componentWillUnmount() {
     ipc.removeAllListeners(FETCHED_TASKS);
+  }
+
+  setActiveDown() {
+    const { tasks, activeId } = this.state;
+    const ids = tasks.map(t => t.id);
+    const activeIndex = ids.indexOf(activeId);
+    const maxIndex = ids[ids.length - 1];
+    const nextActiveIndex = activeIndex + 1;
+    const nextActiveId = ids[nextActiveIndex];
+    if (nextActiveIndex >= maxIndex) {
+      this.setActive(ids[0]);
+    } else {
+      this.setActive(nextActiveId);
+    }
+  }
+
+  setActiveUp() {
+    const { tasks, activeId } = this.state;
+    const ids = tasks.map(t => t.id);
+    const activeIndex = ids.indexOf(activeId);
+    const prevActiveIndex = activeIndex - 1;
+    const prevActiveId = ids[prevActiveIndex];
+    if (prevActiveIndex < 0) {
+      console.log(prevActiveIndex);
+      this.setActive(ids[ids.length - 1]);
+    } else {
+      this.setActive(prevActiveId);
+    }
   }
 
   setActive(id) {
