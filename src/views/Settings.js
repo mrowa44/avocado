054a5f9dd0faa527766ead29cf99caf4033014aa
Justyna Rachowicz <mrowa44@gmail.com>
2017-07-17 19:34:06 +0200

    Settings compoennt

---
+++ src/views/Settings.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './Settings.css';
+
+class Settings extends Component {
+  render() {
+    return (
+      <div>
+        dupa
+      </div>
+    );
+  }
+}
+
+export default Settings;
