803027c8d588f68e14dd8c77903c3360dd4dd508
Justyna Rachowicz <mrowa44@gmail.com>
2017-07-17 19:49:57 +0200

    Basic settings win

--- src/views/Settings.js
+++ src/views/Settings.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 
+import Button from '../components/Button';
 import './Settings.css';
 
 class Settings extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activeBtn: 'general',
+    };
+    this.showGeneral = this.showGeneral.bind(this);
+    this.showIntegrations = this.showIntegrations.bind(this);
+  }
+
+  showGeneral() {
+    this.setState({ activeBtn: 'general' });
+  }
+
+  showIntegrations() {
+    this.setState({ activeBtn: 'integrations' });
+  }
+
   render() {
+    const activeBtn = this.state.activeBtn;
+
     return (
-      <div>
-        dupa
+      <div className="window settings">
+        <header className="toolbar toolbar-header settings__header">
+          <h1 className="title">Settings</h1>
+        </header>
+        <div className="btn-group settings__buttons">
+          <Button
+            text="General"
+            kind={activeBtn === 'general' ? 'primary' : 'default'}
+            onClick={this.showGeneral}
+          />
+          <Button
+            text="Integrations"
+            kind={activeBtn === 'integrations' ? 'primary' : 'default'}
+            onClick={this.showIntegrations}
+          />
+        </div>
       </div>
     );
   }
