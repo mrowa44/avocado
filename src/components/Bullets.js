d4f6fa652dae0fd40bd4468833b0ef9d8f315ffc
Justyna Rachowicz <mrowa44@gmail.com>
2017-08-14 16:02:09 +0200

    Fix bullets bug

--- src/components/Bullets.js
+++ src/components/Bullets.js
@@ -1,58 +1,23 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import cx from 'classnames';
 
 import './Bullets.css';
 
 class Bullets extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isChanging: false,
-    };
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.done !== nextProps.done) {
-      this.setState({ isChanging: true });
-      this.timer = setTimeout(() => {
-        this.setState({ isChanging: false });
-      }, 2000);
-    }
-  }
-
-  componentWillUnmount() {
-    clearTimeout(this.timer);
-  }
-
   render() {
     const { done, remaining } = this.props;
     const left = Math.max(remaining, 0);
-    const doneClassName = cx('bullets__done', {
-      'bullets__done--changing': this.state.isChanging,
-    });
+    const sum = left + done;
 
     return (
       <div className="bullets">
-        {done ?
-          <div className={doneClassName}>
-            {
-              [...Array(done)].map(() => (<div
-                className="bullets__bullet bullets__bullet--done"
-              />))
+        {
+          [...Array(sum)].map((_, index) => {
+            if (index < done) {
+              return <div className="bullets__bullet bullets__bullet--done" key={index} />;
             }
-          </div>
-          : null
-        }
-        {remaining ?
-          <div className="bullets__remaining">
-            {
-              [...Array(left)].map(() => (<div
-                className="bullets__bullet bullets__bullet--remaining"
-              />))
-            }
-          </div>
-          : null
+            return <div className="bullets__bullet bullets__bullet--remaining" key={index} />;
+          })
         }
       </div>
     );
