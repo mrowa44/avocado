446adedfac33f1875eca54e4574bc44b482759bd
Justyna Rachowicz <mrowa44@gmail.com>
2017-08-14 17:41:50 +0200

    Better initial tasks text

--- public/constants.js
+++ public/constants.js
@@ -42,6 +42,6 @@ module.exports = {
   INITIAL_TASKS: [
     { text: 'This is a task, try marking it done by clicking the checkbox', done: false, id: 1 },
     { text: 'You can set a task to focus with CMD + F when it\'s highlighted', done: false, id: 2 },
-    { text: 'Then start a timer :)', done: false, id: 3 },
+    { text: 'Then start a timer and get to work', done: false, id: 3 },
   ],
 };
