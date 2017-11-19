a4fa109707a76f02462afba7ab8ffc684a3f67fc
Justyna Rachowicz <mrowa44@gmail.com>
2017-11-19 13:20:02 +0100

    Hide integrations

--- src/views/Settings.js
+++ src/views/Settings.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 import Button from '../components/Button';
 import SettingsGeneral from '../components/SettingsGeneral';
-import Integrations from '../components/Integrations';
 import './Settings.css';
 
 class Settings extends Component {
@@ -12,15 +11,10 @@ class Settings extends Component {
       activeBtn: 'general',
     };
     this.showGeneral = this.showGeneral.bind(this);
-    this.showIntegrations = this.showIntegrations.bind(this);
   }
 
   showGeneral() {
     this.setState({ activeBtn: 'general' });
-  }
-
-  showIntegrations() {
-    this.setState({ activeBtn: 'integrations' });
   }
 
   render() {
@@ -37,14 +31,8 @@ class Settings extends Component {
             kind={activeBtn === 'general' ? 'primary' : 'default'}
             onClick={this.showGeneral}
           />
-          <Button
-            text="Integrations"
-            kind={activeBtn === 'integrations' ? 'primary' : 'default'}
-            onClick={this.showIntegrations}
-          />
         </div>
         {activeBtn === 'general' && <SettingsGeneral /> }
-        {activeBtn === 'integrations' && <Integrations />}
       </div>
     );
   }
